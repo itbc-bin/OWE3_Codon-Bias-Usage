b1288fcae034e66f7f9f1d15aac6e9208173636c
WendyNavis <71081969+WendyNavis@users.noreply.github.com>
2021-02-25 13:23:58 +0100

    Functies

---
+++ Codon-Bias-Usage.py
@@ -0,0 +1,67 @@
+def extract_data(file):
+    """Reads Fasta file, saves the header and sequence
+    for every gene in a list
+
+    return: Gene_seq - list
+    """
+
+
+def define_groups():
+    """Divides the genes into two groups, one for surface proteins
+    and the other for internal proteins
+
+    return: surface_gene - list
+    return: internal_gene - list
+    """
+
+
+def codons():
+    """Makes a dictionary which can be used for codon analysis
+
+    return: codon_dict - dict
+    """
+    codon_dict = {'ttt': 'F', 'tct': 'S', 'tat': 'Y', 'tgt': 'C',
+                  'ttc': 'F', 'tcc': 'S', 'tac': 'Y', 'tgc': 'C',
+                  'tta': 'L', 'tca': 'S', 'taa': '*', 'tga': '*',
+                  'ttg': 'L', 'tcg': 'S', 'tag': '*', 'tgg': 'W',
+                  'ctt': 'L', 'cct': 'P', 'cat': 'H', 'cgt': 'R',
+                  'ctc': 'L', 'ccc': 'P', 'cac': 'H', 'cgc': 'R',
+                  'cta': 'L', 'cca': 'P', 'caa': 'Q', 'cga': 'R',
+                  'ctg': 'L', 'ccg': 'P', 'cag': 'Q', 'cgg': 'R',
+                  'att': 'I', 'act': 'T', 'aat': 'N', 'agt': 'S',
+                  'atc': 'I', 'acc': 'T', 'aac': 'N', 'agc': 'S',
+                  'ata': 'I', 'aca': 'T', 'aaa': 'K', 'aga': 'R',
+                  'atg': 'M', 'acg': 'T', 'aag': 'K', 'agg': 'R',
+                  'gtt': 'V', 'gct': 'A', 'gat': 'D', 'ggt': 'G',
+                  'gtc': 'V', 'gcc': 'A', 'gac': 'D', 'ggc': 'G',
+                  'gta': 'V', 'gca': 'A', 'gaa': 'E', 'gga': 'G',
+                  'gtg': 'V', 'gcg': 'A', 'gag': 'E', 'ggg': 'G'
+                  }
+
+    return codon_dict
+
+
+def calc_bias():
+    """
+
+    return:
+    """
+
+
+def calc_usage():
+    """
+
+    return:
+    """
+
+
+def make_graph():
+    """
+
+    return:
+    """
+
+
+if __name__ == '__main__':
+    file = "file_name"
+    extract_data(file)
